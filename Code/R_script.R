f212e754095d1d972dfeb99a164eb131ab9677d6
Andrea Cerasoli <andrea.cerasoli03@gmail.com>
2026-02-26 15:55:15 +0100

    Add initial R script with assignments and plots
    Initial R script with basic assignments, arrays, plotting, and package installations.

---
+++ Code/R_script.R
@@ -0,0 +1,36 @@
+# First R script
+
+2 + 3
+
+# oggetti e assegnazione 
+michele <- 2 + 3
+michele = 2 + 3
+
+tecla <- 4 + 6
+
+#array o vettori
+sonia <- c(10, 8, 3, 1, 0)   #funzione e argomenti
+
+giorgia <- c(2, 10, 20, 50, 100)
+
+plot(giorgia, sonia, xlab = "pollution", ylab = "dolphins", cex = 2)
+
+# installazione pacchetti
+# CRAN
+install.packages("Terra")
+library(Terra)
+install.packages("imageRy")
+library(imageRy)
+
+# GitHub 
+install.packages("devtools") #remotes
+library(devtools) #remotes
+instal_GitHub("ducciorocchini/imageRy")
+im.list()
+
+
+
+
+
+
+
