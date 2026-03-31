1b5847c43e7a5b5e0f962ddbd88d4da6ebba8451
Andrea Cerasoli <andrea.cerasoli03@gmail.com>
2026-03-31 23:20:57 +0200

    Update 04_multitemporal_analysis_TEMP.R

--- Code/04_multitemporal_analysis_TEMP.R
+++ Code/04_multitemporal_analysis_TEMP.R
@@ -1,6 +1,8 @@
 library(imageRy)
 library(terra)
 library(viridis)
+library(ggplot2)
+library(ggridges)
 
 im.list() #mi dice le cose che ho in questa funzione -> tutti i file già scaricati
 
@@ -36,21 +38,36 @@ im.multiframe(1,2)
 plot(gr[[1]],col=plasma(100))
 plot(gr[[4]],col=plasma(100))
 
-dif = 
+grdif = gr[[4]] - gr[[1]] # 2015 - 2000
+plot(grdif)
 
 im.plotRGB(gr,r=1,2,4)
 
+#lezione del 31/03
+im.list()
+#caricare tutti i file con quella parte di nome in comune 
+ndvi = im.import("Sentinel2_NDVI") 
+plot(ndvi, col=plasma(100))
+hist(ndvi)
+#i ridge line plots ottengo per ogni mappa come varia una zona in un periodo
+library(ggridges)
+im.ridgeline(ndvi, scale=2, palette=viridis) #lui mi dà un solo plot -> lo sovrascrive ogni volta
+names(ndvi) = c("02_feb", "05_may", "08_aug", "11_nov")
+im.ridgeline(ndvi, scale=1, palette=viridis)
 
-
-
-
-
-
-
-
-
-
-
+pairs(ndvi) #corr plot
+ndvi
+plot(ndvi[[1]],ndvi[[2]])
+# y = x
+# y = a + bx
+# y = 0 + 1x = x
+# a = 0
+# b = 1
+#aggiungiamo una linea rosssa bisettrice 1°- 3° quadrante
+abline(0, 1, col="red")
+# non è la bisettrice 1°- 3° quadrante perché devo ettare i limiti del grafico
+plot(ndvi[[1]],ndvi[[2]], xlim=c(-0.3,0.9), ylim=c(-0.3,0.9))
+abline(0, 1, col="red")
 
 
 
