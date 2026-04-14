aad5700827c49161326fcd3a88365914ce1a2156
Andrea Cerasoli <andrea.cerasoli03@gmail.com>
2026-04-14 17:37:32 +0200

    Update 05_importing_data.R

--- Code/05_importing_data.R
+++ Code/05_importing_data.R
@@ -31,29 +31,56 @@ plotRGB(stack, r=3, g=2, b=1, stretch="lin")   #il NIR è la 3° banda
 plotRGB(stack, r=3, g=2, b=1, stretch="hist")
 #stretch = "hist" aumenta la discriminazione dei valori intermedi -> perde però i valori estremi (max e min) e quindi non converrebbe per la vegetazione
 
+# NDVI
+ndvi = im.ndvi(stack,3,2)
 
+plot(ndvi, col=magma(100))
+plot(ndvi, col=plasma(20))
 
+# esportare i dati in tiff
+writeRaster(ndvi, "ndvi.tif") #funzione sensibile alle maiuscole
+#ora appare nella mia cartella che ho settato all'inizio
+list.files()
+ndvi2=rast( "ndvi.tif"  )  #proviamo a ricaricarla
+im.multiframe(1,2)
+plot(ndvi)
+plot(ndvi2) #sono uguali
 
+#exporting a figure (no data)!
+png("figura1.png")   #file immagine con risoluzione prestabilita bassa
+im.multiframe(2,2)
+plot(green)
+plot(red)
+plot(NIR)
+plot(ndvi)
+dev.off()
 
+#oppure PDF -> file vettoriale con risoluzione non predefinita e scritte perfette 
+pdf("figura1.pdf")   
+im.multiframe(2,2)
+plot(green)
+plot(red)
+plot(NIR)
+plot(ndvi)
+dev.off()
 
+#utilizzo il pacchetto "patchwork"
+p1 = im.ggplot(ndvi)
+p2= im.ridgeline(ndvi, scale = 1)
+p1 + p2
 
+png("figura2.png") 
+p1 + p2
+dev.off()   #non funziona perché bisogna mettersi lì a lavorare con le altezze (vedi pacchetto)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#importare dati direttamente da GitHub su R
+#copio il link
+# https://github.com/ducciorocchini/Telerilevamento_2026/tree/main/Drone2
+# edito "github" in "raw.githubusercontent" ed elimino "tree"
+# https://raw.githubusercontent.com/ducciorocchini/Telerilevamento_2026/main/Drone2
+green_github = rast("https://raw.githubusercontent.com/ducciorocchini/Telerilevamento_2026/main/Drone2/DJI_20260409152942_0001_MS_G.TIF")
+green_github = flip(green_github)
+plot(green_github)
 
 
 
