17bbfd4a98bb6c1c98ff6603cf76906c879af70f
Andrea Cerasoli <andrea.cerasoli03@gmail.com>
2026-04-23 15:45:37 +0200

    Create 08_functions.R

---
+++ Code/08_functions.R
@@ -0,0 +1,47 @@
+# My functions
+porco_dio = function(x,y){
+  z = x + y
+  return(z)
+  }
+
+# make a function that says porca madonna
+semplicemente_ciao = function(x,y){
+  z = x - y
+  return(z)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
