3423ff0c22ebe27a5db0058c78ace236c82a5377
Andrea Cerasoli <andrea.cerasoli03@gmail.com>
2026-07-09 17:54:52 +0200

    Create 09_ESAME.R

---
+++ Code/09_ESAME.R
@@ -0,0 +1,12 @@
+# PROGETTO DI TELERILEVAMENTO IN R - ANDREA CERASOLI
+
+# ANALISI TEMPORALE PRE E POST ERUZIONE SULL'ISOLA DI STROMBOLI
+
+# Ho scaricato immagini Sentinel_2 di Stromboli dal browser open source dell'ESA Copernicus
+# Ho preferito scaricare le immagini da Copernicus invece che da GEE per avere maggiore controllo e scelta sull'immagine 
+# da scaricare: trattandosi di un vulcano, le condizioni di visibilità e copertura nuvolosa, spesso nelle fasi successive a un'eruzione
+# sono spesso inadatte a svolgere un analisi temporale
+
+# Devo ritagliare le immagini e creare uno stack manualmente
+
+
