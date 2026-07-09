ac572c6909e468cd804eb2b58803185c1421b6f5
Andrea Cerasoli <andrea.cerasoli03@gmail.com>
2026-07-09 23:42:49 +0200

    Update 09_ESAME.R

--- Code/09_ESAME.R
+++ Code/09_ESAME.R
@@ -32,6 +32,53 @@ b3_agosto <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.
 b4_agosto <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B04_10m.jp2")
 b8_agosto <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B08_10m.jp2")
 
+# Confrontiamo giugno e agosto per essere sicuri che si le due immagini siano proiettate nello stesso
+# sistema di riferimento e che si sovrappongano alla perfezione
+
+crs(b2_giugno)
+# "PROJCRS[\"WGS 84 / UTM zone 33N\",\n
+crs(b2_agosto) 
+# "PROJCRS[\"WGS 84 / UTM zone 33N\",\n
+
+ext(b2_giugno)
+# SpatExtent : 499980, 609780, 4190220, 4300020 (xmin, xmax, ymin, ymax)
+ext(b2_agosto)
+# SpatExtent : 499980, 609780, 4190220, 4300020 (xmin, xmax, ymin, ymax)
+
+# DEFINIZIONE ESTENSIONE E RITAGLIO (CROP)
+
+# Ordine richiesto dalla funzione ext(): xmin, xmax, ymin, ymax
+estensione_stromboli <- ext(515715, 521500, 4291265, 4296385)
+
+# RITAGLIO (CROP) DI TUTTE LE BANDE DI GIUGNO
+
+b2_giu_crop <- crop(b2_giugno, estensione_stromboli)
+b3_giu_crop <- crop(b3_giugno, estensione_stromboli)
+b4_giu_crop <- crop(b4_giugno, estensione_stromboli)
+b8_giu_crop <- crop(b8_giugno, estensione_stromboli)
+
+# RITAGLIO (CROP) DI TUTTE LE BANDE DI AGOSTO
+
+b2_ago_crop <- crop(b2_agosto, estensione_stromboli)
+b3_ago_crop <- crop(b3_agosto, estensione_stromboli)
+b4_ago_crop <- crop(b4_agosto, estensione_stromboli)
+b8_ago_crop <- crop(b8_agosto, estensione_stromboli)
+
+# CREAZIONE DEGLI STACK MULTI-BANDA E VISUALIZZAZIONE RGB
+
+# Uniamo le bande nell'ordine classico: B2 (1), B3 (2), B4 (3), B8 (4)
+str_giu <- c(b2_giu_crop, b3_giu_crop, b4_giu_crop, b8_giu_crop)
+str_ago <- c(b2_ago_crop, b3_ago_crop, b4_ago_crop, b8_ago_crop)
+
+# Controllo veloce della struttura dello stack in console
+str_giu
+str_ago
+
+# VISUALIZZAZIONE IN COLORI REALI (True Color - RGB 3,2,1)
+par(mfrow=c(1,2))
+plotRGB(str_giu, r=3, g=2, b=1, stretch="lin", main="Stromboli Giugno - Colori Reali")
+plotRGB(str_ago, r=3, g=2, b=1, stretch="lin", main="Stromboli Agosto - Colori Reali")
+
 
 
 
