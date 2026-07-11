de8b5fcf6a5329599f1b8a254af3d347da9c6745
Andrea Cerasoli <andrea.cerasoli03@gmail.com>
2026-07-11 22:48:26 +0200

    Update 09_ESAME.R

--- Code/09_ESAME.R
+++ Code/09_ESAME.R
@@ -21,28 +21,38 @@ library(reshape2)   # Riorganizzazioni dei dati tabellari
 setwd("C:/Users/User/Desktop/telerilevamento in R/progetto esame Stromboli")
 
 # GIUGNO - PRE
-b2_giugno <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B02_10m.jp2") # uso la funzione rast per caricare il percorso
-b3_giugno <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B03_10m.jp2")
-b4_giugno <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B04_10m.jp2")
-b8_giugno <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B08_10m.jp2")
+b2_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B02_10m.jp2")
+b3_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B03_10m.jp2")
+b4_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B04_10m.jp2")
+b8_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B08_10m.jp2")
 
 # AGOSTO - POST
-b2_agosto <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B02_10m.jp2")
-b3_agosto <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B03_10m.jp2")
-b4_agosto <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B04_10m.jp2")
-b8_agosto <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B08_10m.jp2")
+b2_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B02_10m.jp2")
+b3_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B03_10m.jp2")
+b4_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B04_10m.jp2")
+b8_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B08_10m.jp2")
 
-# Confrontiamo giugno e agosto per essere sicuri che si le due immagini siano proiettate nello stesso
+# GIUGNO 2020 - RECUPERO (?)
+b2_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R10m/T33SWD_20200626T095029_B02_10m.jp2")
+b3_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R10m/T33SWD_20200626T095029_B03_10m.jp2")
+b4_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R10m/T33SWD_20200626T095029_B04_10m.jp2")
+b8_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R10m/T33SWD_20200626T095029_B08_10m.jp2")
+
+# Confrontiamo giugno, agosto e giugno 2020 per essere sicuri che le tre immagini siano proiettate nello stesso
 # sistema di riferimento e che si sovrappongano alla perfezione
 
-crs(b2_giugno)
+crs(b2_giu19)
 # "PROJCRS[\"WGS 84 / UTM zone 33N\",\n
-crs(b2_agosto) 
+crs(b2_ago19) 
+# "PROJCRS[\"WGS 84 / UTM zone 33N\",\n
+crs(b2_giu20)
 # "PROJCRS[\"WGS 84 / UTM zone 33N\",\n
 
-ext(b2_giugno)
+ext(b2_giu19)
 # SpatExtent : 499980, 609780, 4190220, 4300020 (xmin, xmax, ymin, ymax)
-ext(b2_agosto)
+ext(b2_ago19)
+# SpatExtent : 499980, 609780, 4190220, 4300020 (xmin, xmax, ymin, ymax)
+ext(b2_giu20)
 # SpatExtent : 499980, 609780, 4190220, 4300020 (xmin, xmax, ymin, ymax)
 
 # DEFINIZIONE ESTENSIONE E RITAGLIO (CROP)
@@ -50,39 +60,49 @@ ext(b2_agosto)
 # Ordine richiesto dalla funzione ext(): xmin, xmax, ymin, ymax
 estensione_stromboli <- ext(515715, 521500, 4291265, 4296385)
 
-# RITAGLIO (CROP) DI TUTTE LE BANDE DI GIUGNO
+# RITAGLIO (CROP) DI TUTTE LE BANDE DI GIUGNO 2019
 
-b2_giu_crop <- crop(b2_giugno, estensione_stromboli)
-b3_giu_crop <- crop(b3_giugno, estensione_stromboli)
-b4_giu_crop <- crop(b4_giugno, estensione_stromboli)
-b8_giu_crop <- crop(b8_giugno, estensione_stromboli)
+b2_giu19_crop <- crop(b2_giu19, estensione_stromboli)
+b3_giu19_crop <- crop(b3_giu19, estensione_stromboli)
+b4_giu19_crop <- crop(b4_giu19, estensione_stromboli)
+b8_giu19_crop <- crop(b8_giu19, estensione_stromboli)
 
-# RITAGLIO (CROP) DI TUTTE LE BANDE DI AGOSTO
+# RITAGLIO (CROP) DI TUTTE LE BANDE DI AGOSTO 2019
 
-b2_ago_crop <- crop(b2_agosto, estensione_stromboli)
-b3_ago_crop <- crop(b3_agosto, estensione_stromboli)
-b4_ago_crop <- crop(b4_agosto, estensione_stromboli)
-b8_ago_crop <- crop(b8_agosto, estensione_stromboli)
+b2_ago19_crop <- crop(b2_ago19, estensione_stromboli)
+b3_ago19_crop <- crop(b3_ago19, estensione_stromboli)
+b4_ago19_crop <- crop(b4_ago19, estensione_stromboli)
+b8_ago19_crop <- crop(b8_ago19, estensione_stromboli)
+
+# RITAGLIO (CROP) DI TUTTE LE BANDE DI GIUGNO 2020
+
+b2_giu20_crop <- crop(b2_giu20, estensione_stromboli)
+b3_giu20_crop <- crop(b3_giu20, estensione_stromboli)
+b4_giu20_crop <- crop(b4_giu20, estensione_stromboli)
+b8_giu20_crop <- crop(b8_giu20, estensione_stromboli)
 
 # CREAZIONE DEGLI STACK MULTI-BANDA E VISUALIZZAZIONE RGB
 
-# Uniamo le bande nell'ordine classico: B2 (1), B3 (2), B4 (3), B8 (4)
-str_giu <- c(b2_giu_crop, b3_giu_crop, b4_giu_crop, b8_giu_crop)
-str_ago <- c(b2_ago_crop, b3_ago_crop, b4_ago_crop, b8_ago_crop)
+# Uniamo le bande nell'ordine: B2 (1), B3 (2), B4 (3), B8 (4)
+str_giu_19 <- c(b2_giu19_crop, b3_giu19_crop, b4_giu19_crop, b8_giu19_crop)
+str_ago_19 <- c(b2_ago19_crop, b3_ago19_crop, b4_ago19_crop, b8_ago19_crop)
+str_giu_20 <- c(b2_giu20_crop, b3_giu20_crop, b4_giu20_crop, b8_giu20_crop)
 
 # Controllo veloce della struttura dello stack in console
-str_giu
-str_ago
+str_giu_19
+str_ago_19
+str_giu_20
 
 # VISUALIZZAZIONE IN COLORI REALI (True Color - RGB 3,2,1)
-par(mfrow=c(1,2))
-plotRGB(str_giu, r=3, g=2, b=1, stretch="lin", main="Stromboli Giugno - Colori Reali")
-plotRGB(str_ago, r=3, g=2, b=1, stretch="lin", main="Stromboli Agosto - Colori Reali")
+par(mfrow=c(1,3))
+plotRGB(str_giu_19, r=3, g=2, b=1, stretch="lin", main="Giugno 2019 (Pre)")
+plotRGB(str_ago_19, r=3, g=2, b=1, stretch="lin", main="Agosto 2019 (Post)")
+plotRGB(str_giu_20, r=3, g=2, b=1, stretch="lin", main="Giugno 2020 (Recupero)")
 
-
-
-
-
+# VISUALIZZAZIONE IN FALSI COLORI (RGB 4,3,2)
+plotRGB(str_giu_19, r=4, g=3, b=2, stretch="lin", main="Falsi Colori - Giugno 2019")
+plotRGB(str_ago_19, r=4, g=3, b=2, stretch="lin", main="Falsi Colori - Agosto 2019")
+plotRGB(str_giu_20, r=4, g=3, b=2, stretch="lin", main="Falsi Colori - Giugno 2020")
 
 
 
