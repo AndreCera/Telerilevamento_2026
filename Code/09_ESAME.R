65dd42377035f46231ccae5b305c6b9b85fcc382
Andrea Cerasoli <andrea.cerasoli03@gmail.com>
2026-07-11 23:29:41 +0200

    Update 09_ESAME.R

--- Code/09_ESAME.R
+++ Code/09_ESAME.R
@@ -4,7 +4,7 @@
 
 # Ho scaricato immagini Sentinel_2 di Stromboli dal browser open source dell'ESA Copernicus
 # Ho preferito scaricare le immagini da Copernicus invece che da GEE per avere maggiore controllo e scelta sull'immagine 
-# da scaricare: trattandosi di un vulcano, le condizioni di visibilità e copertura nuvolosa, spesso nelle fasi successive a un'eruzione
+# da scaricare: trattandosi di un vulcano, le condizioni di visibilità e copertura nuvolosa, spesso nelle fases successive a un'eruzione
 # sono spesso inadatte a svolgere un analisi temporale
 
 # Devo ritagliare le immagini e creare uno stack manualmente
@@ -15,7 +15,7 @@ library(viridis)    # Palette di colori
 library(ggplot2)    # Pacchetto per la creazione di grafici
 library(reshape2)   # Riorganizzazioni dei dati tabellari
 
-# IMPOSTAZIONE WORKSPACE E CARICAMENTO BANDE A 10 METRI
+# IMPOSTAZIONE WORKSPACE E CARICAMENTO BANDE A 10 METRI (E 20 METRI PER B12)
 
 # Configurazione della cartella principale di lavoro (Working Directory)
 setwd("C:/Users/User/Desktop/telerilevamento in R/progetto esame Stromboli")
@@ -25,18 +25,24 @@ b2_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.S
 b3_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B03_10m.jp2")
 b4_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B04_10m.jp2")
 b8_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R10m/T33SWC_20190607T095031_B08_10m.jp2")
+# Caricamento della Banda 12 (SWIR) a 20 metri per Giugno 2019
+b12_giu19 <- rast("S2A_MSIL2A_20190607T095031_N0500_R079_T33SWC_20221219T154232.SAFE/GRANULE/L2A_T33SWC_A020668_20190607T095825/IMG_DATA/R20m/T33SWC_20190607T095031_B12_20m.jp2")
 
 # AGOSTO - POST
 b2_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B02_10m.jp2")
 b3_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B03_10m.jp2")
 b4_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B04_10m.jp2")
 b8_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R10m/T33SWC_20190811T095039_B08_10m.jp2")
+# Caricamento della Banda 12 (SWIR) a 20 metri per Agosto 2019
+b12_ago19 <- rast("S2B_MSIL2A_20190811T095039_N0500_R079_T33SWC_20230505T051258.SAFE/GRANULE/L2A_T33SWC_A012689_20190811T095702/IMG_DATA/R20m/T33SWC_20190811T095039_B12_20m.jp2")
 
 # GIUGNO 2020 - RECUPERO (?)
 b2_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R10m/T33SWD_20200626T095029_B02_10m.jp2")
 b3_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R10m/T33SWD_20200626T095029_B03_10m.jp2")
 b4_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R10m/T33SWD_20200626T095029_B04_10m.jp2")
 b8_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R10m/T33SWD_20200626T095029_B08_10m.jp2")
+# Caricamento della Banda 12 (SWIR) a 20 metri per Giugno 2020
+b12_giu20 <- rast("S2B_MSIL2A_20200626T095029_N0500_R079_T33SWD_20231109T220700.SAFE/GRANULE/L2A_T33SWD_A017265_20200626T095032/IMG_DATA/R20m/T33SWD_20200626T095029_B12_20m.jp2")
 
 # Confrontiamo giugno, agosto e giugno 2020 per essere sicuri che le tre immagini siano proiettate nello stesso
 # sistema di riferimento e che si sovrappongano alla perfezione
@@ -61,37 +67,50 @@ ext(b2_giu20)
 estensione_stromboli <- ext(515715, 521500, 4291265, 4296385)
 
 # RITAGLIO (CROP) DI TUTTE LE BANDE DI GIUGNO 2019
-
 b2_giu19_crop <- crop(b2_giu19, estensione_stromboli)
 b3_giu19_crop <- crop(b3_giu19, estensione_stromboli)
 b4_giu19_crop <- crop(b4_giu19, estensione_stromboli)
 b8_giu19_crop <- crop(b8_giu19, estensione_stromboli)
+# Ritaglio e ricampionamento della Banda 12 sulla griglia a 10m (B2) per Giugno 2019
+b12_giu19_crop <- resample(crop(b12_giu19, estensione_stromboli), b2_giu19_crop, method="bilinear")
 
 # RITAGLIO (CROP) DI TUTTE LE BANDE DI AGOSTO 2019
-
 b2_ago19_crop <- crop(b2_ago19, estensione_stromboli)
 b3_ago19_crop <- crop(b3_ago19, estensione_stromboli)
 b4_ago19_crop <- crop(b4_ago19, estensione_stromboli)
 b8_ago19_crop <- crop(b8_ago19, estensione_stromboli)
+# Ritaglio e ricampionamento della Banda 12 sulla griglia a 10m (B2) per Agosto 2019
+b12_ago19_crop <- resample(crop(b12_ago19, estensione_stromboli), b2_ago19_crop, method="bilinear")
 
 # RITAGLIO (CROP) DI TUTTE LE BANDE DI GIUGNO 2020
-
 b2_giu20_crop <- crop(b2_giu20, estensione_stromboli)
 b3_giu20_crop <- crop(b3_giu20, estensione_stromboli)
 b4_giu20_crop <- crop(b4_giu20, estensione_stromboli)
 b8_giu20_crop <- crop(b8_giu20, estensione_stromboli)
+# Ritaglio e ricampionamento della Banda 12 sulla griglia a 10m (B2) per Giugno 2020
+b12_giu20_crop <- resample(crop(b12_giu20, estensione_stromboli), b2_giu20_crop, method="bilinear")
 
 # CREAZIONE DEGLI STACK MULTI-BANDA E VISUALIZZAZIONE RGB
 
-# Uniamo le bande nell'ordine: B2 (1), B3 (2), B4 (3), B8 (4)
-str_giu_19 <- c(b2_giu19_crop, b3_giu19_crop, b4_giu19_crop, b8_giu19_crop)
-str_ago_19 <- c(b2_ago19_crop, b3_ago19_crop, b4_ago19_crop, b8_ago19_crop)
-str_giu_20 <- c(b2_giu20_crop, b3_giu20_crop, b4_giu20_crop, b8_giu20_crop)
+# Uniamo le bande nell'ordine: B2 (1), B3 (2), B4 (3), B8 (4), B12 (5)
+str_giu_19 <- c(b2_giu19_crop, b3_giu19_crop, b4_giu19_crop, b8_giu19_crop, b12_giu19_crop)
+str_ago_19 <- c(b2_ago19_crop, b3_ago19_crop, b4_ago19_crop, b8_ago19_crop, b12_ago19_crop)
+str_giu_20 <- c(b2_giu20_crop, b3_giu20_crop, b4_giu20_crop, b8_giu20_crop, b12_giu20_crop)
 
 # Controllo veloce della struttura dello stack in console
 str_giu_19
 str_ago_19
 str_giu_20
+
+# Rinominiamo i livelli degli stack per rendere il codice pulito e comprensibile
+names(str_giu_19) <- c("B2", "B3", "B4", "B8", "B12")
+names(str_ago_19) <- c("B2", "B3", "B4", "B8", "B12")
+names(str_giu_20) <- c("B2", "B3", "B4", "B8", "B12")
+
+# plot delle bande 
+plot(str_giu_19, col=plasma(100))
+plot(str_ago_19, col=plasma(100))
+plot(str_giu_20, col=plasma(100))
 
 # VISUALIZZAZIONE IN COLORI REALI (True Color - RGB 3,2,1)
 par(mfrow=c(1,3))
@@ -103,6 +122,7 @@ plotRGB(str_giu_20, r=3, g=2, b=1, stretch="lin", main="Giugno 2020 (Recupero)")
 plotRGB(str_giu_19, r=4, g=3, b=2, stretch="lin", main="Falsi Colori - Giugno 2019")
 plotRGB(str_ago_19, r=4, g=3, b=2, stretch="lin", main="Falsi Colori - Agosto 2019")
 plotRGB(str_giu_20, r=4, g=3, b=2, stretch="lin", main="Falsi Colori - Giugno 2020")
+
 
 
 
